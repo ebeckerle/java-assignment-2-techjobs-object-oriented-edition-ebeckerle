5b8357a1cece3de5451e9436d79a73c2b1c44fee
Elizabeth <elzbthyorke@gmail.com>
2022-05-10 07:48:52 -0500

    "3rd Commit - DRYer JobTest Code"

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -24,17 +24,11 @@ public class JobTest {
     PositionType testPositionType;
     CoreCompetency testCoreCompetency;
 
-//    @Before
-//    public void createTestEmployer(){}
-//
-//    @Before
-//    public void createTestLocation(){}
-//
-//    @Before
-//    public void createTestPositionType(){}
-//
-//    @Before
-//    public void createTestCoreCompetency(){}
+    /* -
+    Created 5 test jobs for the following tests, but found to pass the auto grader I needed to actually
+    create many of them again within the test itself --- Null Pointer Exception
+     */
+
 
     @Before
     public void createTestJobs(){
@@ -49,6 +43,9 @@ public class JobTest {
         testJob5 = new Job(null, new Employer(null), new Location(null), new PositionType(null), new CoreCompetency(null));
     }
 
+    /* - Use Unit testing to verify Parts of the Job class -
+
+     */
     @Test
     public void testSettingJobId(){
         Job testJob1;
@@ -56,6 +53,7 @@ public class JobTest {
         testJob1 = new Job("Backend Developer", new Employer("Workshop Designs"), new Location("St.Louis"), new PositionType("Back-End Developer"), new CoreCompetency("Java"));
         testJob2 = new Job("Backend Developer", new Employer("Workshop Designs"), new Location("St.Louis"), new PositionType("Back-End Developer"), new CoreCompetency("Java"));
         assertEquals(testJob1.getId()+1, testJob2.getId(), 0.01);
+
     }
 
     @Test
@@ -85,6 +83,10 @@ public class JobTest {
         assertFalse(testJob1.equals(testJob2));
     }
 
+
+    /* - Use TDD to design and code a custom toString method for the Job class
+
+     */
     @Test
     public void testJobToString(){
         String expectedString = "\nID: "+testJob1.getId()+"\nName: Backend Developer\nEmployer: Workshop Designs\nLocation: St.Louis\nPosition Type: Back-End Developer\nCore Competency: Java\n";
@@ -92,24 +94,25 @@ public class JobTest {
 
         assertEquals(expectedString, actualString);
 
-        //new Test???
-        String expectedString2 = "\nID: "+testJob4.getId()+"\nName: Product tester\nEmployer: ACME\nLocation: Data not available\nPosition Type: Data not available\nCore Competency: Data not available\n";
-        String actualString2 = testJob4.toString();
-
-        assertEquals(expectedString2, actualString2);
-
-        //new Test???
-        String expectedString3 = "\nOOPS! This job does not seem to exist.\n";
-        String actualString3 = testJob5.toString();
-
-        assertEquals(expectedString3, actualString3);
+//        /* - Originally I incorporated the two following tests within this one test,
+//        but due to the autograder, I did have to break them out to the last test - testToStringHandlesEmptyField
+//         */
+//        //this tests  returns "Data not available" if a field is null  - new Test???
+//        String expectedString2 = "\nID: "+testJob4.getId()+"\nName: Product tester\nEmployer: ACME\nLocation: Data not available\nPosition Type: Data not available\nCore Competency: Data not available\n";
+//        String actualString2 = testJob4.toString();
+//
+//        assertEquals(expectedString2, actualString2);
+//
+//        //this tests  returns "OOPS! This job does not seem to exist" if a field is null -  new Test???
+//        String expectedString3 = "\nOOPS! This job does not seem to exist.\n";
+//        String actualString3 = testJob5.toString();
+//
+//        assertEquals(expectedString3, actualString3);
     }
 
     @Test
     public void testToStringStartsAndEndsWithNewLine(){
         testJob1 = new Job("Backend Developer", new Employer("Workshop Designs"), new Location("St.Louis"), new PositionType("Back-End Developer"), new CoreCompetency("Java"));
-
-        String expectedString = "\nID: "+testJob1.getId()+"\nName: Backend Developer\nEmployer: Workshop Designs\nLocation: St.Louis\nPosition Type: Back-End Developer\nCore Competency: Java\n";
         String actualString = testJob1.toString();
 
         char firstChar = actualString.charAt(0);
@@ -117,35 +120,22 @@ public class JobTest {
 
         //one assert equals for the first \n
         assertEquals(firstChar, '\n');
-        assertEquals(expectedString, actualString);
+
         //one assert equals for the last \n
         assertEquals(lastChar, '\n');
-        assertEquals(expectedString, actualString);
+
 
     }
 
     @Test
     public void testToStringContainsCorrectLabelsAndData(){
         testJob1 = new Job("Backend Developer", new Employer("Workshop Designs"), new Location("St.Louis"), new PositionType("Back-End Developer"), new CoreCompetency("Java"));
-        testJob4 = new Job("Product tester", new Employer("ACME"), new Location(null), new PositionType(null), new CoreCompetency(null));
-        testJob5 = new Job(null, new Employer(null), new Location(null), new PositionType(null), new CoreCompetency(null));
 
         String expectedString = "\nID: "+testJob1.getId()+"\nName: Backend Developer\nEmployer: Workshop Designs\nLocation: St.Louis\nPosition Type: Back-End Developer\nCore Competency: Java\n";
         String actualString = testJob1.toString();
 
         assertEquals(expectedString, actualString);
 
-        //new Test???
-        String expectedString2 = "\nID: "+testJob4.getId()+"\nName: Product tester\nEmployer: ACME\nLocation: Data not available\nPosition Type: Data not available\nCore Competency: Data not available\n";
-        String actualString2 = testJob4.toString();
-
-        assertEquals(expectedString2, actualString2);
-
-        //new Test???
-        String expectedString3 = "\nOOPS! This job does not seem to exist.\n";
-        String actualString3 = testJob5.toString();
-
-        assertEquals(expectedString3, actualString3);
     }
 
     @Test
@@ -155,24 +145,19 @@ public class JobTest {
         testJob5 = new Job(null, new Employer(null), new Location(null), new PositionType(null), new CoreCompetency(null));
         Job testJob6 = new Job("Product tester", new Employer("ACME"), new Location(""), new PositionType(""), new CoreCompetency(""));
 
-        String expectedString = "\nID: "+testJob1.getId()+"\nName: Backend Developer\nEmployer: Workshop Designs\nLocation: St.Louis\nPosition Type: Back-End Developer\nCore Competency: Java\n";
-        String actualString = testJob1.toString();
-
-        assertEquals(expectedString, actualString);
-
-        //new Test???
+        //tests a job with some fields null
         String expectedString2 = "\nID: "+testJob4.getId()+"\nName: Product tester\nEmployer: ACME\nLocation: Data not available\nPosition Type: Data not available\nCore Competency: Data not available\n";
         String actualString2 = testJob4.toString();
 
-
         assertEquals(expectedString2, actualString2);
 
-        //new Test???
+        //tests a job that doesn't exist
         String expectedString3 = "\nOOPS! This job does not seem to exist.\n";
         String actualString3 = testJob5.toString();
 
         assertEquals(expectedString3, actualString3);
 
+        //tests a job with some fields having empty strings
         String expectedString6 = "\nID: "+testJob6.getId()+"\nName: Product tester\nEmployer: ACME\nLocation: Data not available\nPosition Type: Data not available\nCore Competency: Data not available\n";
         String actualString6 = testJob6.toString();
 
